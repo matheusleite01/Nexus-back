3d6189934b6b7c707e4b62dbca5ab1afb08ef9ce
matheusleite01 <de.leite2014@gmail.com>
2024-08-16 13:49:32 -0300

    feat: add app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from "express";
+import routes from "./routes";
+import connectDatabase from "./db/dbConfig";
+
+connectDatabase();
 
 const app = express();
+routes(app);
+
 
 export default app;
